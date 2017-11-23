cea491b52908d7a39641137aed3683012b421931
Shrinidhi <shrinidhi.hegde@nineleaps.com>
2017-11-23 16:16:57 +0530

    initial project setup

---
+++ src/main/java/com/shr/poc/traceid/TraceidApplication.java
@@ -0,0 +1,12 @@
+package com.shr.poc.traceid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceidApplication.class, args);
+	}
+}
